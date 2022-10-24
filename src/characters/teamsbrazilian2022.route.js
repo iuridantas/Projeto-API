0a298281f4963d6296b4217b57bef8179dcb8fd4
Iuri Dantas <uri.dantas@hotmail.com>
2022-10-24 15:57:24 -0300

    feat: creating auth and users in swagger.json

--- src/characters/teamsbrazilian2022.route.js
+++ src/characters/teamsbrazilian2022.route.js
@@ -9,9 +9,9 @@ route.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
 route.get('/characters',controllerteamsbrazilian2022.findALLteamsbrazilian2022controller,);
 route.get('/characters/find/:id',validId,controllerteamsbrazilian2022.findByIdteambrazilian2022controller,);
-route.post('/characters/create',validobjectBody,controllerteamsbrazilian2022.createteamsbrazilian2022controller,);
-route.put('/characters/update/:id',validId,validobjectBody,controllerteamsbrazilian2022.updateteamsbrazilian2022controller,);
-route.delete('/characters/delete/:id',validId,controllerteamsbrazilian2022.deleteteamsbrazilian2022controller,);
-route.get('/characters/search',controllerteamsbrazilian2022.findsearchteamsbrazilian2022controller,);
+route.post('/characters/create',authmiddleware,validobjectBody,controllerteamsbrazilian2022.createteamsbrazilian2022controller,);
+route.put('/characters/update/:id',authmiddleware,validId,validobjectBody,controllerteamsbrazilian2022.updateteamsbrazilian2022controller,);
+route.delete('/characters/delete/:id',authmiddleware,validId,controllerteamsbrazilian2022.deleteteamsbrazilian2022controller,);
+route.get('/characters/search',authmiddleware,controllerteamsbrazilian2022.findsearchteamsbrazilian2022controller,);
 
 module.exports = route;
