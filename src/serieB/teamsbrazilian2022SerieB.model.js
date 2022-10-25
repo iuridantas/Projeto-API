e295aaeb0ec158a246141e3286d00e66f27b5ff7
Iuri Dantas <uri.dantas@hotmail.com>
2022-10-25 13:57:40 -0300

    fix: fixing mongoDB error

--- src/serieB/teamsbrazilian2022SerieB.model.js
+++ src/serieB/teamsbrazilian2022SerieB.model.js
@@ -1,6 +1,6 @@
 const mongooose = require('mongoose');
 
-const teamsbrazilian2022SerieASchema= new mongooose.Schema({
+const teamsbrazilian2022SerieBSchema= new mongooose.Schema({
   name: { type: String, required: true },
   foundation: { type: Number, required: true },
   shield: { type: String, required: true },
@@ -8,6 +8,6 @@ const teamsbrazilian2022SerieASchema= new mongooose.Schema({
   localization: { type: String, required: true },
 });
 
-const teamsbrazilian2022SerieB = mongooose.model('Serie_B',teamsbrazilian2022SerieASchema,);
+const teamsbrazilian2022SerieB = mongooose.model('Serie_B',teamsbrazilian2022SerieBSchema,);
 
 module.exports = teamsbrazilian2022SerieB;
--- src/serieC/teamsbrazilian2022SerieC.model.js
+++ src/serieC/teamsbrazilian2022SerieC.model.js
@@ -1,6 +1,6 @@
 const mongooose = require('mongoose');
 
-const teamsbrazilian2022SerieASchema= new mongooose.Schema({
+const teamsbrazilian2022SerieCSchema= new mongooose.Schema({
   name: { type: String, required: true },
   foundation: { type: Number, required: true },
   shield: { type: String, required: true },
@@ -8,6 +8,6 @@ const teamsbrazilian2022SerieASchema= new mongooose.Schema({
   localization: { type: String, required: true },
 });
 
-const teamsbrazilian2022SerieC = mongooose.model('Serie_B',teamsbrazilian2022SerieASchema,);
+const teamsbrazilian2022SerieC = mongooose.model('Serie_C',teamsbrazilian2022SerieCSchema,);
 
 module.exports = teamsbrazilian2022SerieC;
